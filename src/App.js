1b017a21939a419ee0d860d6c4a9b6ea81833744
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-08-31 20:18:54 +0700

    Saving a solution I am trying

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
-import PartListBody, { body } from './components/PartListBody';
+import PartList from './components/PartList';
+import Avatar from './components/Avatar';
 
-function App(body) {
+function App() {
+  const [bodySrc, setBodySrc] = useState("");
+  const [clothes1Src, setClothes1Src] = useState("");
+  const [clothes2Src, setClothes2Src] = useState("");
+  const [clothes3Src, setClothes3Src] = useState("");
+  const [eyebrowsSrc, setEyebrowsSrc] = useState("");
+  const [eyesSrc, setEyesSrc] = useState("");
+  const [glassesSrc, setGlassesSrc] = useState("");
+  const [hairSrc, setHairSrc] = useState("");
+  const [hatSrc, setHatSrc] = useState("");
+  const [mouthSrc, setMouthSrc] = useState("");
+  const [neckwearSrc, setNeckwearSrc] = useState("");
+
   return (
     <>
       <div id="title">
@@ -11,87 +25,8 @@ function App(body) {
         <div className="line2">👗 👒 👜 🎀 Customization 🧢 👖 👔 👞 </div>
       </div>
       <div id="content">
-        <div className="avatar-group">
-          <div className="avatar">
-            <img src="../images/body/1.png" alt="Why" />
-            <img src="../images/eyes/1.png" alt="" />
-            <img src="../images/eyebrows/1.png" alt="" />
-            <img src="../images/mouths/1.png" alt="" />
-            <img src="../images/noses/1.png" alt="" />
-            <img src="../images/hair/1.png" alt="" />
-            <img src="../images/facial_hair/2.png" alt="" />
-            <img src="../images/accessories/earrings/2.png" alt="" />
-            <img src="../images/accessories/glasses/2.png" alt="" />
-            <img src="../images/accessories/hats/2.png" alt="" />
-            <img src="../images/accessories/neckwear/2.png" alt="" />
-            <img src="../images/clothes/layer_2/1.png" alt="" />
-          </div>
-          <div className="button">
-            <button>RANDOMIZE</button>
-          </div>
-        </div>
-        <div id="parts">
-          <div className="part-section">
-            <div className="part-header">Body</div>
-            <div className="part-list">
-              {body.map((obj) => (
-                <PartListBody key={obj} source={obj} />
-              ))}
-            </div>
-          </div>
-          <div className="part-section">
-            <div className="part-header">Eyes</div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-          <div className="part-section">
-            <div className="part-header">Hair</div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-
-          <div className="part-section">
-            <div className="part-header">Mouth</div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-          <div className="part-section">
-            <div className="part-header"></div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-          <div className="part-section">
-            <div className="part-header"></div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-
-          <div className="part-section">
-            <div className="part-header"></div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-
-          <div className="part-section">
-            <div className="part-header"></div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-
-          <div className="part-section">
-            <div className="part-header"></div>
-            <div className="part-list">
-              <img className="part-square" src="../images/body/1.png" alt="Can't"></img>
-            </div>
-          </div>
-        </div>
+        <Avatar />
+        <PartList /*Render list of selection parts*/ />
       </div>
     </>
   );
